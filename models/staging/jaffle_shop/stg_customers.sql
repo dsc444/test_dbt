2a88ba1e169cd15336035d9ef534d07694698165
Darragh Collins <collinsdarragh9@gmail.com>
2025-12-17 09:18:53 +0000

    big commit

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,9 +1,13 @@
+{{ config (
+    materialized='ephemeral'
+)}}
+
 with customers as (
     select
         id as customer_id,
         first_name,
         last_name
-    from raw.jaffle_shop.customers
+    from {{source('jaffle_shop', 'customers')}}
 )
 
 select * from customers

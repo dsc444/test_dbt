3ab412735f24ad359cf11a1d04c662b9a6e74584
CodingLime464 <128549198+dsc444@users.noreply.github.com>
2025-12-23 10:43:47 +0000

    Update local_snap.sql

--- snapshots/local_snap.sql
+++ snapshots/local_snap.sql
@@ -4,12 +4,13 @@
     config(
         target_database='FIVETRAN2',
         target_schema='snapshots',
-        unique_key='id',
+        unique_key='ad_id',
         strategy='check',
         check_cols='all'
     )
 }}
 
 select * from {{ source('fivetran_sheets', 'locations') }}
+
 
 {% endsnapshot %}
